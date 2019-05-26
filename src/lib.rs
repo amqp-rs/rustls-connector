6ee6068112a135f248c1f4f7aa12763165cffe51
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2019-05-26 17:29:05 +0200

    add mid handshake WouldBlock handling
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/lib.rs
+++ src/lib.rs
@@ -12,13 +12,16 @@ pub use webpki;
 pub use webpki_roots;
 
 use failure::{Backtrace, Context, Fail};
-use rustls::{ClientConfig, ClientSession, Session, StreamOwned as TlsStream};
+use rustls::{ClientConfig, ClientSession, Session, StreamOwned};
 
 use std::{
     fmt,
     io::{self, Read, Write},
     sync::Arc,
 };
+
+/// A TLS stream
+pub type TlsStream<S> = StreamOwned<ClientSession, S>;
 
 /// The connector
 pub struct RustlsConnector {
@@ -35,25 +38,69 @@ impl Default for RustlsConnector {
     }
 }
 
+macro_rules! perform_handshake (
+    ($session: expr, $stream: expr) => (
+        if let Err(e) = $session.complete_io(&mut $stream) {
+            return Err(if e.kind() == io::ErrorKind::WouldBlock {
+                ErrorKind::HandshakeWouldBlock(MidHandshakeTlsStream{ session: $session, stream: $stream })
+            } else {
+                ErrorKind::IOError(e)
+            }.into());
+        }
+        return Ok(TlsStream::new($session, $stream));
+    );
+);
+
 impl RustlsConnector {
     /// Connect to the given host
-    pub fn connect<S: Read + Write>(&self, mut stream: S, domain: &str) -> Result<TlsStream<ClientSession, S>, Error> {
+    pub fn connect<S: fmt::Debug + Read + Write + Send + Sync>(&self, mut stream: S, domain: &str) -> Result<TlsStream<S>, Error<S>> {
         let mut session = ClientSession::new(&self.config, webpki::DNSNameRef::try_from_ascii_str(domain).map_err(|()| ErrorKind::InvalidDomainName(domain.to_owned()))?);
-        // FIXME: handle MidHandshake/WouldBlock
-        session.complete_io()?;
-        Ok(TlsStream::new(session, stream))
+        perform_handshake!(session, stream);
+    }
+}
+
+/// A TLS stream which has been interrupted during the handshake
+#[derive(Debug)]
+pub struct MidHandshakeTlsStream<S: Read + Write> {
+    session: ClientSession,
+    stream:  S,
+}
+
+impl<S: fmt::Debug + Read + Write + Send + Sync + 'static> MidHandshakeTlsStream<S> {
+    /// Get a reference to the inner stream
+    pub fn get_ref(&self) -> &S {
+        &self.stream
+    }
+
+    /// Get a mutable reference to the inner stream
+    pub fn get_mut(&mut self) -> &S {
+        &mut self.stream
+    }
+
+    /// Retry the handshake
+    pub fn handshake(mut self) -> Result<TlsStream<S>, Error<S>> {
+        perform_handshake!(self.session, self.stream);
+    }
+}
+
+impl<S: Read + Write> fmt::Display for MidHandshakeTlsStream<S> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "MidHandshakeTlsStream")
     }
 }
 
 /// The type of error that can be returned in this crate.
 #[derive(Debug)]
-pub struct Error {
-    inner: Context<ErrorKind>,
+pub struct Error<S: fmt::Debug + Read + Write + Send + Sync + 'static> {
+    inner: Context<ErrorKind<S>>,
 }
 
 /// The different kinds of errors that can be reported.
 #[derive(Debug, Fail)]
-pub enum ErrorKind {
+pub enum ErrorKind<S: fmt::Debug + Read + Write + Send + Sync + 'static> {
+    /// We hit a WouldBlock during handshake
+    #[fail(display = "WouldBlock during handshake")]
+    HandshakeWouldBlock(MidHandshakeTlsStream<S>),
     /// An invalid domain name
     #[fail(display = "Invalid domain name: {}", _0)]
     InvalidDomainName(String),
@@ -65,20 +112,20 @@ pub enum ErrorKind {
     __Nonexhaustive,
 }
 
-impl Error {
+impl<S: fmt::Debug + Read + Write + Send + Sync> Error<S> {
     /// Return the underlying ErrorKind
-    pub fn kind(&self) -> &ErrorKind{
+    pub fn kind(&self) -> &ErrorKind<S> {
         self.inner.get_context()
     }
 }
 
-impl fmt::Display for Error {
+impl<S: fmt::Debug + Read + Write + Send + Sync + 'static> fmt::Display for Error<S> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         self.inner.fmt(f)
     }
 }
 
-impl Fail for Error {
+impl<S: fmt::Debug + Read + Write + Send + Sync + 'static> Fail for Error<S> {
     fn cause(&self) -> Option<&dyn Fail> {
         self.inner.cause()
     }
@@ -88,19 +135,19 @@ impl Fail for Error {
     }
 }
 
-impl From<ErrorKind> for Error {
-    fn from(kind: ErrorKind) -> Self {
+impl<S: fmt::Debug + Read + Write + Send + Sync> From<ErrorKind<S>> for Error<S> {
+    fn from(kind: ErrorKind<S>) -> Self {
         Context::new(kind).into()
     }
 }
 
-impl From<Context<ErrorKind>> for Error {
-    fn from(inner: Context<ErrorKind>) -> Self {
+impl<S: fmt::Debug + Read + Write + Send + Sync> From<Context<ErrorKind<S>>> for Error<S> {
+    fn from(inner: Context<ErrorKind<S>>) -> Self {
         Error { inner }
     }
 }
 
-impl From<io::Error> for Error {
+impl<S: fmt::Debug + Read + Write + Send + Sync + 'static> From<io::Error> for Error<S> {
     fn from(io: io::Error) -> Self {
         ErrorKind::IOError(io).into()
     }
