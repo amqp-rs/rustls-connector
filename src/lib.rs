4674296aee613fc147f7aacc4f3977d586564d74
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-02-03 10:54:17 +0100

    v0.8.2
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![deny(missing_docs)]
 #![warn(rust_2018_idioms)]
-#![doc(html_root_url = "https://docs.rs/rustls-connector/0.8.1/")]
+#![doc(html_root_url = "https://docs.rs/rustls-connector/0.8.2/")]
 
 //! # Connector similar to openssl or native-tls for rustls
 //!
