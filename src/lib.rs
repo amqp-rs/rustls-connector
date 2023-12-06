074ac31d892d6e8e40879eac9dcf22bd2aa0586f
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2023-12-06 10:14:02 +0100

    v0.19.2

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![deny(missing_docs)]
 #![warn(rust_2018_idioms)]
-#![doc(html_root_url = "https://docs.rs/rustls-connector/0.19.1/")]
+#![doc(html_root_url = "https://docs.rs/rustls-connector/0.19.2/")]
 
 //! # Connector similar to openssl or native-tls for rustls
 //!
